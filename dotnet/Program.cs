7c59289d715f3cc5249ff0a18f2df237886b4354
ilkerispir <ilker.ispir@alo-tech.com>
2021-11-22 00:24:57 +0300

    feat: added dotnet app

---
+++ dotnet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace helloworld_csharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            string url = String.Concat("http://0.0.0.0:", "8080");
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().UseUrls(url);
+                });
+        }
+    }
+}
